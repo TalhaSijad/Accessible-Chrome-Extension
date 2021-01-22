c2daa958528a0e3fe6deb425995e589fe6a9de9c
TalhaSijad <talha.sijad@abzorb.co.uk>
2021-01-22 20:36:06 +0000

    Added external script files such as jQuery and Cookie.js

--- popup.js
+++ popup.js
@@ -1,3 +1,32 @@
-$('#darkMode').click(function () {
-    alert('grthh');
+// $(document).ready(function () {
+//     function modifyDOM() {
+    //     if ($('#darkMode').checked) {
+    //         document.body.style.backgroundColor = "red";
+    //     } else {
+    //         document.body.style.backgroundColor = "green";
+    //     }
+    // }
+
+    // $('#darkMode').click(function () {
+    //     chrome.tabs.executeScript({
+    //         code: 'Cookies.set("Dark-Mode-for-Liverpool-FC-News", "Enabled")'
+    //     });
+    // });
+// });
+
+$(document).ready(function () {
+    // console.log("ready to go la")
+    // Cookies.set('TESTTTT', 'VALUEEEEE')
+
+    $('#darkMode').click(function () {
+        if ($(this).is(':checked')) {
+            document.body.style.backgroundColor = "red";
+        } else {
+            document.body.style.backgroundColor = "green";
+        }
+
+        // chrome.tabs.executeScript({
+        //     code: 'Cookies.set("Dark-Mode-for-Liverpool-FC-News", "Enabled")'
+        // });
+    });
 });
