8ffd109505291ae590206b3e37060c23a7893023
TalhaSijad <talha.sijad@abzorb.co.uk>
2021-02-05 20:17:46 +0000

    Added logic for highlighting buttons and inputs on the active page.

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  // If there is a Cookie stored for Dark Mode.
   if (!!Cookies.get('Dark-Mode-for-Accessibility-Assistant', 'Enabled')) {
+    // Toggle the dark attribute against the body tag to trigger the CSS.
     document.body.classList.toggle('dark');
   }
 });
@@ -14,12 +16,54 @@ function toggleDarkMode() {
   location.reload();
 }
 
+function toggleElementHighlight(opt) {
+  if (opt === 1) {
+    var inputs = document.getElementsByTagName('input');
+
+    for (i = 0; i < inputs.length; i++) {
+        inputs[i].setAttribute("style", "border: 3px solid green");
+    }  
+  } else {
+    var buttons = document.getElementsByTagName('button');
+
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute("style", "border: 3px solid red");
+    }  
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('toggleDarkMode');
-  checkPageButton.addEventListener('click', function() {
+  // Dark Mode:
+  var toggleDarkModeBtn = document.getElementById('toggleDarkMode');
+
+  toggleDarkModeBtn.addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
       chrome.tabs.executeScript({
         code: '('  + toggleDarkMode + ')()'
+      });
+    });
+  }, false);
+
+  // Highlighting Elements:
+  var highlightElementPicker = document.getElementById('highlightElementPicker');
+
+  highlightElementPicker.addEventListener('change', function() {
+    let opt = $('#highlightElementPicker').find(":selected").val()
+
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.executeScript({
+        code: '('  + toggleElementHighlight + ')(' + opt + ')'
+      });
+    });
+  }, false);
+
+  // Reset Button:
+  var resetElementPicker = document.getElementById('resetElements');
+
+  resetElementPicker.addEventListener('click', function() {
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.executeScript({
+        code: 'location.reload()'
       });
     });
   }, false);
