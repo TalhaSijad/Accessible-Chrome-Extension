e18aa479f7584786b5cb9e8696960efb66abcca5
TalhaSijad <talha.sijad@abzorb.co.uk>
2021-03-18 19:32:47 +0000

    Implemented support for reading parent elements such as nav and h1.

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ function toggleDarkMode() {
   if (!!Cookies.get('Dark-Mode-for-Accessibility-Assistant', 'Enabled')) {
     Cookies.remove('Dark-Mode-for-Accessibility-Assistant');
   } else {
-    Cookies.set('Dark-Mode-for-Accessibility-Assistant', 'Enabled')
+    Cookies.set('Dark-Mode-for-Accessibility-Assistant', 'Enabled');
   }
 
   location.reload();
@@ -103,6 +103,36 @@ document.addEventListener('DOMContentLoaded', function() {
   }, false);
 }, false);
 
+// document.addEventListener("visibilitychange", function() {
+//   if (document.visibilityState === 'visible') {
+//     console.log("hello?")
+//     let title = new SpeechSynthesisUtterance("loaded mate");
+//     window.speechSynthesis.speak(title);
+//   }
+// });
+
+// function handleVisibilityChange() {
+//   if(document.hidden) {
+//     // the page is hidden
+//   } else {
+//     readPageTitle();
+//   }
+// }
+
+// $("#tabs").tabs({
+//   select: function(event, ui) {
+//       alert("PRESSED TAB!");
+//   }
+// });
+
+// document.addEventListener("visibilitychange", handleVisibilityChange, false);
+
+function readPageTitle() {
+  speechSynthesis.cancel();
+  let title = new SpeechSynthesisUtterance(document.title);
+  window.speechSynthesis.speak(title);
+}
+
 // Screen Reader:
 $(document).keyup(function(e) {
   speechSynthesis.cancel();
@@ -112,11 +142,32 @@ $(document).keyup(function(e) {
   if (!!Cookies.get('Screen-Reader-for-Accessibility-Assistant', 'Enabled')) {
     // If the button pressed is the Tab key, or Shift+Tab.
     if (e.key == 'Tab') {
+      // Highlight the current element.
       $(e.target).css('box-shadow', '0 0 5px 5px #ffcb3d');
 
+      // Remove the highlight after 1 second.
       setTimeout(function () {
         $(e.target).css('box-shadow', '');
       }, 1000);
+
+      // The following checks are for parent tags.
+      if ($(e.target).parents('nav').length) {
+        element += "Navigation. ";
+      } else if ($(e.target).parents('li').length) {
+        element += "List item. ";
+      } else if ($(e.target).parents('h1').length) {
+        element += "Heading 1. ";
+      } else if ($(e.target).parents('h2').length) {
+        element += "Heading 2. ";
+      } else if ($(e.target).parents('h3').length) {
+        element += "Heading 3. ";
+      } else if ($(e.target).parents('h4').length) {
+        element += "Heading 4. ";
+      } else if ($(e.target).parents('h5').length) {
+        element += "Heading 5. ";
+      } else if ($(e.target).parents('h6').length) {
+        element += "Heading 6. ";
+      }
 
       if ($(e.target).attr('aria-label')) {
         element += $(e.target).attr('aria-label');
@@ -124,12 +175,22 @@ $(document).keyup(function(e) {
         element += $(e.target).attr('title');
       } else if ($('label[for="' + $(e.target).attr('id') + '"]').text()) {
         element += $('label[for="' + $(e.target).attr('id') + '"]').text();
+      } else if ($(e.target).attr('alt')) {
+        element += $(e.target).attr('alt');
+      } else if ($(e.target).children("img").attr('alt')) {
+        element += $(e.target).children("img").attr('alt');
       } else {
         element += $(e.target).text();
       }
 
       if ($(e.target).attr('placeholder')) {
         element += " with hint " + $(e.target).attr('placeholder');
+      } else if ($(e.target).attr('aria-expanded')) {
+        if ($(e.target).attr('aria-expanded') == "true") {
+          element += ". Collapsible with state collapsed";
+        } else {
+          element += ". Collapsible with state uncollapsed";
+        }
       }
 
       if ($(e.target)[0].nodeName == "A") {
@@ -138,13 +199,12 @@ $(document).keyup(function(e) {
         element += ". Input";
       } else if ($(e.target)[0].nodeName == "BUTTON" || $(e.target).attr('role') == 'button') {
         element += ". Button";
-        // Check if it has an aria-expanded, if it does, its a dropdown so read out the state
       }
-
-      // Add another else if for "select"
 
       let readElement = new SpeechSynthesisUtterance(element);
       window.speechSynthesis.speak(readElement);
+    } else if (e.key == "ArrowUp" || e.key == "ArrowDown") {
+      console.log($(e.target)[0].value)  // ToDo
     }
   }
 
